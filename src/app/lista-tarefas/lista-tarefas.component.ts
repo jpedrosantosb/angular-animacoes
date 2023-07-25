04facb0966511446c95e9ee1390f821579b180df
jpedrosantosb <jpedrosantosb@gmail.com>
2023-07-25 18:04:34 -0300

    Buscando tarefas

--- src/app/lista-tarefas/lista-tarefas.component.ts
+++ src/app/lista-tarefas/lista-tarefas.component.ts
@@ -20,6 +20,8 @@ export class ListaTarefasComponent implements OnInit {
   validado: boolean = false;
   indexTarefa = -1;
   id: number = 0;
+  campoBusca: string = ''
+  tarefasFiltradas: Tarefa[] = [];
 
   formulario: FormGroup = this.fomBuilder.group({
     id: [0],
@@ -38,8 +40,19 @@ export class ListaTarefasComponent implements OnInit {
   ngOnInit(): Tarefa[] {
     this.service.listar(this.categoria).subscribe((listaTarefas) => {
       this.listaTarefas = listaTarefas;
+      this.tarefasFiltradas = listaTarefas;
     });
-    return this.listaTarefas;
+    return this.tarefasFiltradas;
+  }
+
+  filtrarTarefasPorDescricao(descricao: string) {
+    this.campoBusca = descricao.trim().toLowerCase();
+    if (descricao) {
+      this.tarefasFiltradas = this.listaTarefas.filter(tarefa =>
+        tarefa.descricao.toLowerCase().includes(this.campoBusca));
+    } else {
+      this.tarefasFiltradas = this.listaTarefas;
+    }
   }
 
   mostrarOuEsconderFormulario() {
@@ -122,7 +135,7 @@ export class ListaTarefasComponent implements OnInit {
 
   listarAposCheck() {
     this.service.listar(this.categoria).subscribe((listaTarefas) => {
-      this.listaTarefas = listaTarefas;
+      this.tarefasFiltradas = listaTarefas;
     });
   }
 
